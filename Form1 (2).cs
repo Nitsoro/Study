4260b7a8a1ab981dabbe0faaac4cff2d8217ecdf
Klimentii Koloshnicin <koltkolor1312@gmail.com>
2020-10-20 09:06:44 +0630

    1

---
+++ Form1 (2).cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Opl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            int k = 0;
+            int a;
+            a = Convert.ToInt32(textBox1.Text);
+            for (k=0;a<k;k++)
+            {
+                int b = a % 10;
+                if (b==3)
+
+            }
+        }
+    }
+}
